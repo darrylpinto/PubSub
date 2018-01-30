e52f36cd26808cc0ad1e8b8ff74b894bc291cba2
renzil7 <renzil1994@gmail.com>
2018-01-30 01:41:22 -0500

    Constructor for Topic.java created

--- pubsubFiles/PubSubDemo/src/edu/rit/CSCI652/demo/Topic.java
+++ pubsubFiles/PubSubDemo/src/edu/rit/CSCI652/demo/Topic.java
@@ -6,4 +6,13 @@ public class Topic {
 	private int id;
 	private List<String> keywords;
 	private String name;
+
+	public Topic(int id, String name){
+		this.id = id;
+		this.name = name;
+	}
+
+	public addKeyword(String keyword){
+		//Adding a keyword to the topic
+	}
 }
