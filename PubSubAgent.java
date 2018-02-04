a7e03fe46fce867905f2a89837632d748dadd481
Darryl Pinto <dp6417@rit.edu>
2018-02-04 02:34:39 -0500

    Revert "Separating Receiver and Sender"
    This reverts commit 5f2206f0dc6f1f2ecb4dfbc083795e4a9703bb4a.

--- PubSubAgent.java
+++ PubSubAgent.java
@@ -152,3 +152,135 @@ public class PubSubAgent implements Publisher, Subscriber{
 //
 
 }
+
+class Sender implements Runnable {
+	private PubSubAgent pubSub;
+
+	public Sender(PubSubAgent pubsub){
+		this.pubSub = pubsub;
+	}
+	@Override
+	public void run() {
+
+		try {
+			while(true) {
+				printMenu();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+	private void printMenu() throws IOException {
+
+		System.out.println("1. Subscribe");
+		System.out.println("2. Unsubscribe");
+		System.out.println("3. List subscribed topics");
+		System.out.println("4. Publish");
+		System.out.println("5. Advertise");
+		System.out.println("6. Read Messages");
+
+		Scanner sc  = new Scanner(System.in);
+
+		System.out.println("Enter choice");
+		int choice = sc.nextInt();
+
+		switch (choice){
+			case 1:
+//
+//				System.out.println("Press 1 to subscribe by TOPIC\n"+
+//						"Press 2 to subscribe by KEYWORD\n");
+//
+//				int choice2 = sc.nextInt();
+//				if(choice2 == 1){
+//					System.out.println("\n==== SUBSCRIPTION BY TOPIC ====");
+//					ArrayList<String> interestedTopics = pubSub.getTopics();
+//
+//					this.pubSub.output.writeUTF(""+interestedTopics.size());
+//					this.pubSub.output.flush();
+//
+//					this.pubSub.output.writeUTF(this.pubSub.username);
+//					this.pubSub.output.flush();
+//
+//					for (String interestedTopic : interestedTopics) {
+//						this.pubSub.subscribe(new Topic(0, interestedTopic));
+//					}
+//				}
+//				else{
+//					System.out.println("\n==== SUBSCRIPTION BY KEYWORD ====");
+//
+//					// KEYWORD SUBSCRIPTION
+//				}
+//
+//				break;
+
+			case 2:
+				// UNSUBCRIBE LOGIC
+				break;
+
+			// 3
+			// 4
+			case 5:
+				Random rand = new Random();
+				int id = rand.nextInt();
+				System.out.println("Name of the topic:");
+				String topicName = sc.next();
+
+				Topic newTopic = new Topic(id,topicName);
+
+				pubSub.advertise(newTopic);
+				break;
+
+			default:
+				System.out.println("Invalid ENTRY! TRY AGAIN");
+		}
+	}
+}
+
+class Receiver implements Runnable{
+	PubSubAgent pubSub;
+
+	public Receiver(PubSubAgent pubSub){
+		this.pubSub = pubSub;
+	}
+
+	//Have to declare a new
+	@Override
+	public void run() {
+
+		while(true) {
+			try {
+				String choice = this.pubSub.input.readUTF();
+
+				if (choice.equals("Topic"))
+					this.receiveAdvertisements();
+				else if (choice.equals("Event"))
+					this.receiveEvents();
+				else
+					System.out.println("Error");
+			} catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+	}
+
+	private void receiveAdvertisements() throws IOException, ClassNotFoundException {
+
+		//Implement receiving advertisements here
+		Object obj = this.pubSub.input.readObject();
+		Topic topic = (Topic)obj;
+		System.out.println("===============================================");
+		System.out.println("**New Advertisement Received :"+topic.getName());
+		System.out.println("===============================================");
+
+	}
+
+	private void receiveEvents(){
+
+		//implement receiving event notifications here
+
+	}
+}
